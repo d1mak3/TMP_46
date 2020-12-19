fc98444cbe276fc7cce09665cffd1a5aa42bf14d
d1mak3 <d1mak3@yandex.ru>
2020-12-19 16:33:14 +0300

    Поправил папку

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace TMP_46
+{
+  class Program
+	{ 
+		static Stack FlipStack(ref Stack stackToFlip) // Переворачиваем стек
+		{
+			Stack reversedStack = new Stack();
+			while (stackToFlip.Size > 0)
+				reversedStack.Push(stackToFlip.Pop);
+			return reversedStack;
+		}
+
+		static void NaturalMerge(ref Stack stackToSort)
+		{
+			Stack sorted = new Stack(); // Сюда мы будем записывать часть стека, которая уже отсортирована			
+			int pop = stackToSort.Pop;
+			sorted.Push(pop);
+			while(stackToSort.Size > 0)
+			{
+				if (sorted.Back < stackToSort.Back)
+				{					
+					pop = stackToSort.Pop;
+					sorted.Push(pop);					
+				}
+				else
+					break;
+			}
+			
+			Stack toSort = new Stack(stackToSort);
+			if (toSort.Size > 0)
+			{
+				NaturalMerge(ref toSort); // Рекурсивно разбиваем на сортированные отрезки оставшуюся часть стека				
+			}
+			
+			sorted = FlipStack(ref sorted);
+			
+			stackToSort.Clear(); // Очищаем стек, чтобы записать в него сортированные данные
+			
+			while (sorted.Size > 0 && toSort.Size > 0) // Заносим, параллельно сортируя, элементы обратно в основной стек
+			{				
+				if (sorted.Back < toSort.Back)
+					stackToSort.Push(sorted.Pop);
+				else
+					stackToSort.Push(toSort.Pop);						
+			}
+			while (sorted.Size > 0)
+				stackToSort.Push(sorted.Pop);
+			while (toSort.Size > 0)
+				stackToSort.Push(toSort.Pop);
+			stackToSort = FlipStack(ref stackToSort);
+		} 
+
+		static void Main()
+		{
+			Console.WriteLine("Write count of numbers:");
+			int countOfNumbers = Convert.ToInt32(Console.ReadLine());
+			Random rand = new Random();
+
+			Stack stackToSort = new Stack();
+
+			for (int i = 0; i < countOfNumbers; ++i)
+			{
+				stackToSort.Push(rand.Next(-50, 100));
+			}
+			//stackToSort.Push(6); stackToSort.Push(5); stackToSort.Push(3); stackToSort.Push(4);
+
+			Console.WriteLine("Original stack:");
+
+			Stack output = new Stack(stackToSort);
+			int size = output.Size;
+			for (int i = 0; i < size; ++i)
+			{
+				Console.Write($"{output.Pop}\t");
+			}
+
+			NaturalMerge(ref stackToSort);
+			
+			Console.WriteLine("\nSorted stack:");
+
+			size = stackToSort.Size;
+			for (int i = 0; i < size; ++i)
+			{
+				Console.Write($"{stackToSort.Pop}\t");
+			}
+		}
+	}
+}
---
+++ Stack.cs
@@ -0,0 +1,86 @@
+﻿namespace TMP_46
+{
+  class Stack
+  {
+    private int[] _stack = new int[0]; // Стек
+
+    public Stack()
+		{
+
+		}
+
+    public Stack(Stack _oldStack) // Копируем стек в наш стек
+    {
+      int size = _oldStack.Size;
+      Stack temp = new Stack();
+      for (int i = 0; i < size; ++i)
+      {
+        int newNum = _oldStack.Pop;
+        temp.Push(newNum);
+      }
+
+      size = temp.Size;
+      for (int i = 0; i < size; ++i)
+      {
+        int newNum = temp.Pop;
+        this.Push(newNum);
+        _oldStack.Push(newNum);
+      }
+    }
+
+    public int Size // Размер стека
+    {
+      get
+      {
+        int size = 0;
+
+        foreach (int o in _stack) // Прибавляем 1 к размеру за каждый элемент в массифе
+        {
+          ++size;
+        }
+
+        return size;
+      }
+    }
+
+    public void Push(int newElement) // Добавление элемента в конец стека
+    {
+      int[] temp = _stack; // Сохраняем элементы во временный стек
+
+      _stack = new int[Size + 1]; // Делаем наш массив на 1 больше
+      _stack[0] = newElement;
+
+      for (int i = 1; i < Size; ++i) // Переписываем элементы обратно
+        _stack[i] = temp[i - 1];
+    }
+
+    public int Pop // Вывод и удаление последнего элемента
+    {
+      get
+      {
+        int popped = _stack[0]; // Сохраняем значение, которое будем выводить
+        int[] temp = _stack; // Сохраняем элементы во временный стек       
+
+        _stack = new int[Size - 1]; // Уменьшаем размер стека на 1 
+
+        for (int i = 0; i < Size; ++i) // Переписываем элементы обратно
+          _stack[i] = temp[i + 1];
+
+        return popped; // Возвращаем сохранённое значение
+      }
+    }
+
+    public int Back // Вывод последнего элемента
+    {
+      get
+      {
+        return _stack[0];
+      }
+    }
+
+    public void Clear() // Очищаем стек
+    {
+      _stack = new int[0];
+    }
+  }
+}
